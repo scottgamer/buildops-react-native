87e54c0ac295bac76751c42683eb764758386d1f
Richard Munoz <richosojason@msn.com>
2019-12-30 08:14:20 -0500

    added navigator

---
+++ src/AppNavigator.js
@@ -0,0 +1,29 @@
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+
+import EmployeesList from "./components/EmployeesList";
+// import SitesDetails from "./components/SiteDetails";
+
+const NavigationStack = createStackNavigator(
+  {
+    EmployeesList: {
+      screen: EmployeesList,
+      navigationOptions: {
+        header: null
+      }
+    },
+    // SitesDetails: {
+    //   screen: SitesDetails,
+    //   navigationOptions: {
+    //     header: null
+    //   }
+    // }
+  },
+  {
+    initialRouteName: "EmployeesList"
+  }
+);
+
+const AppNavigator = createAppContainer(NavigationStack);
+
+export default AppNavigator;
