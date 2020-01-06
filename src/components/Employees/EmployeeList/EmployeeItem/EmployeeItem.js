f02a951fddf78bf22c206194ae929c25cd424b6c
Richard Munoz <richosojason@msn.com>
2020-01-06 07:43:00 -0500

    added employees component

---
+++ src/components/Employees/EmployeeList/EmployeeItem/EmployeeItem.js
@@ -0,0 +1,44 @@
+import React from "react";
+import {} from "react-native";
+
+import "./EmployeeItem.css";
+
+const employeeItem = props => (
+  <li key={props.employeeId} className="events__list-item">
+    <div>
+      <h1>
+        {props.firstname} {props.lastname}
+      </h1>
+
+      {props.skills.map(skill => (
+        <p key={skill._id}>{skill.name}</p>
+      ))}
+
+      {props.addresses.map(address => (
+        <p key={address._id}>
+          {address.line1} & {address.line2}, {address.city}, {address.state}
+        </p>
+      ))}
+    </div>
+    <div>
+      <button
+        className="btn"
+        onClick={() => {
+          props.onUpdate(props.employeeId);
+        }}
+      >
+        Update
+      </button>
+      <button
+        className="btn"
+        onClick={() => {
+          props.onDelete(props.employeeId);
+        }}
+      >
+        Delete
+      </button>
+    </div>
+  </li>
+);
+
+export default employeeItem;
---
+++ src/pages/Employees/Employees.js
@@ -0,0 +1,194 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  SafeAreaView,
+  StatusBar,
+  FlatList,
+  Button
+} from "react-native";
+
+import Separator from "../../components/Separator/Separator";
+import { Spinner } from "react-native-ui-kitten";
+
+import axios from "axios";
+
+const Employees = () => {
+  const [employees, setEmployees] = useState({
+    employees: [],
+    selectedEmployee: null
+  });
+
+  const [isActive, setActive] = useState(true);
+  const [isLoading, setLoading] = useState(true);
+  const [isLoadingMore, setLoadingMore] = useState(false);
+
+  const [isCreating, setCreating] = useState(false);
+  const [isDeleting, setDeleting] = useState(false);
+  const [isUpdating, setUpdating] = useState(false);
+
+  useEffect(() => {
+    fetchEmployees();
+    setActive(false);
+  }, []);
+
+  const fetchEmployees = async () => {
+    try {
+      setLoading(true);
+
+      const requestBody = {
+        query: `
+          query {
+            employees {
+              _id
+              firstname
+              lastname 
+              addresses {
+                _id
+                line1
+                line2
+                city
+                state
+                zipcode
+              }
+              skills {
+                _id
+                name
+              }
+            }
+          }
+        `
+      };
+
+      const response = await axios.post(
+        `http://192.168.100.107:4000/graphql`,
+        JSON.stringify(requestBody),
+        {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+      );
+
+      if (response.status !== 200 && response.status !== 201) {
+        throw new Error("Failed!");
+      }
+
+      const employeesRes = response.data.data.employees;
+
+      if (isActive) {
+        setEmployees({ ...employees, employees: employeesRes });
+        setLoading(false);
+      }
+    } catch (error) {
+      console.log(error);
+      if (isActive) {
+        setLoading(false);
+      }
+    }
+  };
+
+  const renderEmployee = ({ item, index }) => {
+    console.log("item", item);
+    console.log();
+    return (
+      <View>
+        <Text>
+          {item.firstname} {item.lastname}
+        </Text>
+        <Text>{item.skills[0].name}</Text>
+        <Text>
+          {item.addresses[0].line1} & {item.addresses[0].line2},{" "}
+          {item.addresses[0].city}, {item.addresses[0].state}
+        </Text>
+      </View>
+    );
+  };
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
+      <View style={styles.container}>
+        <Text style={styles.containerHeader}>List of Employees</Text>
+        <Button style={styles.buttonAdd} title="Add an Employee" />
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          data={employees.employees}
+          keyExtractor={employee => employee._id}
+          contentContainerStyle={styles.listLayout}
+          renderItem={renderEmployee}
+        />
+      </View>
+      <Separator />
+    </SafeAreaView>
+  );
+};
+
+// class Employees extends Component {
+//   async componentDidMount() {
+//     const data = await this.fetchData();
+//     this.setState({
+//       isLoading: false
+//     });
+//     this.populateSiteData(data.sites);
+//   }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fefefe",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    alignItems: "center"
+  },
+  containerHeader: {
+    padding: 10,
+    fontSize: 18,
+    fontWeight: "500"
+  },
+  listLayout: {
+    padding: 10,
+    marginTop: 10
+  },
+  cardContainer: {
+    flex: 1,
+    flexDirection: "row",
+    width: "100%",
+    height: 110,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 5,
+    marginBottom: 15,
+    borderRadius: 3,
+    borderColor: "black",
+    backgroundColor: "#fff",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 0.75 },
+    shadowOpacity: 0.5,
+    shadowRadius: 1,
+    elevation: 2,
+    alignContent: "center",
+    justifyContent: "center"
+  },
+  itemBgImage: {
+    height: "100%",
+    width: "25%"
+  },
+  nameAndAddressContainer: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    paddingLeft: 10,
+    marginRight: 7
+  },
+  itemNameText: { fontSize: 17, fontWeight: "bold", marginBottom: 15 },
+  flexCenterAll: {
+    display: "flex",
+    width: "100%",
+    height: "100%",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
+export default Employees;
