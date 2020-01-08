453b47bdafa1ab800aabe4b68af8425988e052a3
Richard Muñoz <richard.munoz@crifa.com>
2020-01-08 10:14:41 -0500

    added employee detials screen

--- src/components/Employees/EmployeeList/EmployeeDetails.js
+++ src/components/Employees/EmployeeList/EmployeeDetails.js
@@ -1,11 +1,29 @@
 import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-const EmployeeDetails = (props) => {
+const EmployeeDetails = props => {
+  // console.log(props.navigation);
+
   return (
     <View>
       <Text>Employee details</Text>
+      <Text>{props.navigation.state.params.employee.firstname}</Text>
     </View>
   );
 };
 
 export default EmployeeDetails;
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  headertext: {
+    fontSize: 15,
+    fontWeight: "bold",
+    color: "#fff",
+    marginLeft: 10
+  }
+});
